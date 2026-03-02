3fd8bb41641a9fdc46e55686a37078fe59e774ac
mgk76 <mattiaskarlsson76@gmail.com>
2026-03-02 16:04:15 +0000

    Read CSV

--- Start/03_files/csvfiles_start.py
+++ Start/03_files/csvfiles_start.py
@@ -3,16 +3,29 @@ import csv
 
 
 # TODO: list the dialects that are available to use
-
+print(csv.list_dialects())
+print()
 
 # TODO: Open a CSV file and read each row of data
 def reader_sample():
-    pass
+    with open("StockQuotes.csv") as datafile:
+        reader = csv.reader(datafile)
+        for row in reader:
+            print(row)
 
 
 # TODO: Use the CSV module Sniffer to see what dialect of CSV this is
 def use_sniffer():
-    pass
+    with open("StockQuotes.csv") as datafile:
+        dialect = csv.Sniffer().sniff(datafile.read(1024))
+        datafile.seek(0)
+        hasheader = csv.Sniffer().has_header(datafile.read(1024))
+        datafile.seek(0)
+        print("Headers: ", str(hasheader))
+        print(dialect.delimiter)
+        print(dialect.escapechar)
+        print(dialect.quotechar)
+
 
 
 # TODO: Write data to a CSV file
@@ -22,5 +35,6 @@ def writer_sample():
 
 # Exercise the samples
 # reader_sample()
+
 # writer_sample()
-# use_sniffer()
+use_sniffer()
--- Start/03_files/tempfiles_start.py
+++ Start/03_files/tempfiles_start.py
@@ -3,12 +3,32 @@ import os
 import tempfile
 
 # TODO: get information about the temp data environment
+print("gettempdir: ", tempfile.gettempdir())
+print("gettempprefix: ", tempfile.gettempprefix())
 
 
 # TODO: create a temporary file using mkstemp()
-
+#Return Tuple of file handle and pointer
+(tempfh, tempfp) = tempfile.mkstemp(".tmp", "testTemp", None, True)
+f = os.fdopen(tempfh, "w+t")
+f.write("Some temporary data")
+f.seek(0)
+print(f.read())
+f.close()
+os.remove(tempfp)
+print()
 
 # TODO: create a temp file using the TemporaryFile class
-
-
+with tempfile.TemporaryFile(mode="w+t") as tfp:
+  tfp.write("This is some data")
+  tfp.seek(0)
+  print(tfp.read())
+  
 # TODO: create a temporary directory using the TemporaryDirectory class
+with tempfile.TemporaryDirectory() as tdp:
+  path = os.path.join(tdp, "tempfile.txt")
+  tfp = open(path, "w+t")
+  tfp.write("This is a temp file dir")
+  tfp.seek(0)
+  print(tfp.read())
+  
