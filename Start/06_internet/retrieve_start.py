50d951914c89692a1c1c836563b4349d5f8460b1
mgk76 <mattiaskarlsson76@gmail.com>
2026-03-03 10:30:32 +0000

    internet data

--- Start/06_internet/retrieve_start.py
+++ Start/06_internet/retrieve_start.py
@@ -5,16 +5,26 @@ import urllib.request
 sample_url = "http://httpbin.org/xml"
 
 # TODO: Create a request to retrieve data using urllib.request
-
+response = urllib.request.urlopen(sample_url)
 
 # TODO: Check the status
-status_code = 0
+status_code = response.status
+print(status_code)
 
 
 # TODO: if no error, then read the response contents
 if status_code >= 200 and status_code < 300:
     # TODO: work with response headers
-    pass
+    print(response.getheaders())
+  
 
-    # TODO: read the data from the URL
-    pass
+    print("------------------------------------------------------------------------")
+
+    data = response.read()
+    print(data)
+    
+    print("------------------------------------------------------------------------")
+    
+    data_utf = data.decode('utf-8')
+    print(data_utf)
+
--- Start/06_internet/urls_start.py
+++ Start/06_internet/urls_start.py
@@ -4,20 +4,27 @@ import urllib.parse
 sample_url = "http://server.example.com:8080/example.html?val1=1&val2=Hello+World"
 
 # TODO: parse a URL with urlparse()
-
+result = urllib.parse.urlparse(sample_url)
+print(result)
 
 # TODO: process the query part of the URL with parse_qs()
-
+# returns dict of lists
+querystr = urllib.parse.parse_qs(result.query)
+print(querystr)
 
 # TODO: parse the query string using parse_qsl()
-
+# returns list of tuples
+querystr = urllib.parse.parse_qsl(result.query)
+print(querystr)
 
 # TODO: create the URL with the geturl() method
-
+print(result.geturl)
+print()
 
 # TODO: quote() replaces special characters for use in URLs
 sample_string = "Hello El Niño"
-
+print(urllib.parse.quote(sample_string))
+print(urllib.parse.quote_plus(sample_string))
 
 # TODO: Use urlencode() to convert maps to parameter values
 query_data = {
@@ -25,3 +32,5 @@ query_data = {
     "City": "Anytown USA",
     "Age": 37
 }
+result = urllib.parse.urlencode(query_data)
+print(result)
