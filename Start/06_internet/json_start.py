42dce361d9d8bc07b47d4fc7863329eeb6d56719
mgk76 <mattiaskarlsson76@gmail.com>
2026-03-03 10:56:29 +0000

    json

--- Start/06_internet/json_start.py
+++ Start/06_internet/json_start.py
@@ -4,12 +4,19 @@ import json
 
 
 # TODO: use urllib to retrieve some sample JSON data
-
+req = urllib.request.urlopen("http://httpbin.org/json")
+data = req.read().decode('utf-8')
+print(data)
+print()
 
 # TODO: use the JSON module to parse the returned data
-
+obj = json.loads(data)
 
 # TODO: when the data is parsed, we can access it like any other object
+print(obj["slideshow"]["author"])
+
+for slide in obj["slideshow"]["slides"]:
+  print(slide["title"])
 
 
 # TODO: python objects can also be written out as JSON
@@ -20,3 +27,9 @@ objdata = {
         "Learning Python", "Advanced Python", "Python Standard Library Essential Training"
     ]
 }
+
+jsondata = json.dumps(objdata, indent=4)
+print(jsondata)
+
+with open("jsonoutput.json", "w") as fp:
+  json.dump(objdata, fp, indent=4)
