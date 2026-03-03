41dcaeb18e20bd951cecc4637133ab0a49cecc8b
mgk76 <mattiaskarlsson76@gmail.com>
2026-03-03 09:17:24 +0000

    temp pass

--- Start/04_numbers/temppass_start.py
+++ Start/04_numbers/temppass_start.py
@@ -1,23 +1,40 @@
 # Create a temporary password using Python
 import secrets
 import string
+import random
 
 
 # TODO: Function to return a temporary password given a length
 def generate_temp_pass(num_chars=8):
-    pass
-
+    potential_chars = string.ascii_letters + string.digits + "+=?/!"
+    result = "".join(secrets.choice(potential_chars) for i in range(num_chars))
+    return result
 
 # TODO: Function to return a temporary password and enforce 1 number and 1 uppercase
 def generate_better_pass(num_chars=8):
-    pass
+    potential_chars = string.ascii_letters + string.digits + "+=?/!"
+    passwd = "".join(secrets.choice(potential_chars) for i in range(num_chars-2))
+    passwd += secrets.choice(string.digits)
+    passwd += secrets.choice(string.ascii_uppercase)
+    
+    result = random.sample(passwd, k=len(passwd))
+    random.shuffle(result)
+    passwd = "".join(result)
+    return passwd
+    
+    
 
 # create a temporary password
-# print(generate_temp_pass(10))
+print(generate_temp_pass(10))
+
+print()
 
 # create a stronger temporary password
-# print(generate_better_pass(10))
+print(generate_better_pass(10))
 
 
 # TODO: create a temporary, hard-to-guess URL
+
 result_url = "https://my.example.com?reset="
+result_url += secrets.token_urlsafe(15)
+print(result_url)
