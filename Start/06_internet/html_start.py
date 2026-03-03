587aaad1335858816ea02be0545a6cf1f4eb4d56
mgk76 <mattiaskarlsson76@gmail.com>
2026-03-03 10:46:21 +0000

    parse html

--- Start/06_internet/html_start.py
+++ Start/06_internet/html_start.py
@@ -1,6 +1,7 @@
 # working with HTML data via the HTMLParser
 from html.parser import HTMLParser
 
+metacount = 0
 
 # define a class that will handle various parts of an HTML file
 # create a subclass of HTMLParser and override the handler methods
@@ -8,32 +9,46 @@ class MyHTMLParser(HTMLParser):
     # TODO: handle an opening HTML tag of the form:
     # <tagname attr="value">
     def handle_starttag(self, tag, attrs):
-        pass
+        print("Enc start tag:", tag)
+        global metacount
+        if tag == "meta":
+            metacount += 1
+
+        pos = self.getpos()
+        print("\tAt line:", pos[0], "position", pos[1])
+        if len(attrs) > 0:
+            print("\tAttributes:")
+            for a in attrs:
+                print("\t", a[0], "=", a[1])
 
     # TODO: function to handle the ending tag, which looks like:
     # </tagname>
     def handle_endtag(self, tag):
-        pass
+        print("Encountered an end tag:", tag)
 
     # TODO: function to handle character and text data (tag contents)
     # <tag>this data here in the tag</tag>
     def handle_data(self, data):
-        pass
+        if data.isspace():
+            return
+        print("Encountered text data:", data)
 
     # TODO: function to handle the processing of HTML comments
     # <!-- which look like this -->
     def handle_comment(self, data):
-        pass
+        print("Encountered comment:", data)
 
 
 # TODO: create an instance of the parser
-
+parser = MyHTMLParser()
 
 # open the sample HTML file and read it
 f = open("samplehtml.html")
 if f.mode == "r":
     contents = f.read()  # read the entire file
     # TODO: pass the content to the parser
+    parser.feed(contents)
 
 
 # TODO: Count the number of <meta> tags in the file
+print("Meta Count: ", metacount)
