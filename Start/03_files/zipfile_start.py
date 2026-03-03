84c91a672d384e568ee8ede522273ed785bdb0a4
mgk76 <mattiaskarlsson76@gmail.com>
2026-03-03 08:05:57 +0000

    Working with zip archive

--- Start/03_files/zipfile_start.py
+++ Start/03_files/zipfile_start.py
@@ -3,18 +3,33 @@ import zipfile
 
 
 # TODO: Create a new ZIP archive
+zfile = zipfile.ZipFile("archive.zip", "w")
+zfile.write("file1.txt")
+zfile.write("file2.txt")
+zfile.write("file3.txt")
+zfile.close()
 
 
 # TODO: Check validity of the file
-
+print(zipfile.is_zipfile("archive.zip"))
+print()
 
 # TODO: Read the properties of a ZIP archive
+zfile = zipfile.ZipFile("archive.zip")
+print(zfile.namelist())
+print("-------------------------")
+print(zfile.infolist())
 
+print()
 
 # TODO: Read the properties of ZIP contents
-
+zinfo = zfile.getinfo("file1.txt")
+print(zinfo.file_size)
+print(zinfo.filename)
+print(zfile.read("file1.txt"))
 
 # TODO: Extract ZIP file contents
-
+zfile.extract("file2.txt", "extracted")
+zfile.extractall("extractall")
 
 # TODO: Ensure that the file is closed
